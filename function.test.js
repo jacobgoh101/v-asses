c2cb985a9336f2e8f79a1057ee7038090823ae2e
Jacob Goh <jacob.goh@123rf.com>
2021-03-29 14:09:54 +0800

    change test to ignore array order when comparing result

--- function.test.js
+++ function.test.js
@@ -1,15 +1,31 @@
 const { solution } = require('./solution');
 
+function sortBy(arr, callback = (a) => a) {
+  return arr.slice().sort((a, b) => callback(a) - callback(b));
+}
+
+function compareResultIgnoringOrder(arr1, arr2) {
+  //sort children array
+  arr1 = arr1.map((arr) => sortBy(arr));
+  arr2 = arr2.map((arr) => sortBy(arr));
+  // sort parent array
+  arr1 = sortBy(arr1, (arr) => arr.join(''));
+  arr2 = sortBy(arr2, (arr) => arr.join(''));
+  expect(arr1).toEqual(arr2);
+}
+
 test(`[5, 1, 10, 7], 11`, () => {
-  expect(solution([5, 1, 10, 7], 11)).toEqual([[1, 10]]);
+  compareResultIgnoringOrder(solution([5, 1, 10, 7], 11), [[1, 10]]);
 });
+
 test(`[1, 8, 9, 3, 10, 1], 12`, () => {
-  expect(solution([1, 8, 9, 3, 10, 1], 12)).toEqual([
+  compareResultIgnoringOrder(solution([1, 8, 9, 3, 10, 1], 12), [
     [9, 3],
     [1, 10, 1],
     [8, 3, 1],
   ]);
 });
+
 test(`[1, 3, 9], 3`, () => {
-  expect(solution([1, 3, 9], 3)).toEqual([]);
+  compareResultIgnoringOrder(solution([1, 3, 9], 3), []);
 });
